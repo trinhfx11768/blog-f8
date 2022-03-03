f35f27c96aeaad7bfb4e87708529f3412e2be522
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-02 16:54:19 -0800

    finish css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+//Path cố định
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Template engine
 app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
