53adbe5e6658f8945e29d314c50ed0b6a47e2893
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-04 08:39:16 -0800

    Routes Controller Prettier

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,15 @@
+
+class SiteController {
+
+    //[GET] /Site
+    index(req, res) {
+        res.render('home');
+    }
+
+    search(req, res) {
+        res.render('search');
+    }
+
+}
+
+module.exports = new SiteController;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ const express = require('express');
 const path = require('path');
 const handlebars = require('express-handlebars');
 
+const newsController = require('./app/controllers/NewsController');
+const route = require('./routes');
+
 const app = express();
 const port = 3000;
+
+//Parsed body cho form data, XMLHttp, Json, ...
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //Path cố định
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Template engine
-app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
+app.engine('.hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/search', (req, res) => {
-    console.log(req.query);
-    res.render('search');
-});
+//Route init
+route(app);
 
 app.listen(port);
